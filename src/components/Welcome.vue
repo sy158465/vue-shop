a163de5de1e589c0fc2323af19516037053a9323
Stone Sun <15092930291@163.com>
2021-05-24 17:32:21 +0800

    user

---
+++ src/components/Welcome.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3>Welcome</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {},
+  mounted () { },
+  methods: {}
+}
+</script>
