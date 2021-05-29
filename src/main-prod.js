5411f0424bdfcfe2187ecfa8b3c7998aa41ed1e5
Stone Sun <15092930291@163.com>
2021-05-29 17:26:57 +0800

    完成

---
+++ src/main-prod.js
@@ -0,0 +1,56 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+// import './plugins/element.js'
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
+import axios from 'axios'
+import TreeTable from 'vue-table-with-tree-grid'
+
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+
+// 导入加载进度条包
+import NProgress from 'nprogress'
+
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 在request 拦截其中，展示进度条NProgress.start()
+axios.interceptors.request.use(config => {
+  NProgress.start()
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+// 在response 拦截其中，隐藏NProgress.done()
+axios.interceptors.response.use(config => {
+  NProgress.done()
+  return config
+})
+
+Vue.prototype.$http = axios
+
+Vue.config.productionTip = false
+
+Vue.component('tree-table', TreeTable)
+// 将富文本编辑器注册为全局可用组件
+Vue.use(VueQuillEditor)
+
+// 定义全局时间过滤器
+Vue.filter('dateFormat', function (originVal) {
+  const dt = new Date(originVal * 1000)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getMilliseconds() + '').padStart(2, '0')
+
+  const DateStr = `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+
+  return DateStr
+})
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
