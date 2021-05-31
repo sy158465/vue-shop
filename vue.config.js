37107fc119390a5cac14204e9f4b11d551532cfd
Stone Sun <15092930291@163.com>
2021-05-31 10:41:33 +0800

    add dist

--- vue.config.js
+++ vue.config.js
@@ -22,11 +22,11 @@ module.exports = {
     // 开发模式
     config.when(process.env.NODE_ENV === 'development', config => {
       config.entry('app').clear().add('./src/main-dev.js')
-    })
 
-    config.plugin('html').tap(args => {
-      args[0].isProd = false
-      return args
+      config.plugin('html').tap(args => {
+        args[0].isProd = false
+        return args
+      })
     })
   }
 }
